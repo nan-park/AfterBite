7ca28786474fd2c57005fa0591fd3252431090a0
NanPark <nemyrmain@gmail.com>
2026-01-29 23:14:51 +0900

    Initial Commit

---
+++ AfterBite/AfterBiteApp.swift
@@ -0,0 +1,17 @@
+//
+//  AfterBiteApp.swift
+//  AfterBite
+//
+//  Created by 박난 on 1/29/26.
+//
+
+import SwiftUI
+
+@main
+struct AfterBiteApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AfterBiteTests/AfterBiteTests.swift
@@ -0,0 +1,17 @@
+//
+//  AfterBiteTests.swift
+//  AfterBiteTests
+//
+//  Created by 박난 on 1/29/26.
+//
+
+import Testing
+@testable import AfterBite
+
+struct AfterBiteTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
